2ba158bcf2fef69b85a1b24f01dcb524215938bd
DavidVarga <davidvarga986@gmail.com>
2023-05-19 21:48:31 +0200

    Create Person class

---
+++ src/person/Person.java
@@ -0,0 +1,13 @@
+package person;
+
+public class Person {
+    private String name;
+    private String surname;
+    private final int identificationNumber;
+
+    public Person(String name, String surname, int identificationNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.identificationNumber = identificationNumber;
+    }
+}
